8cf4564ad034f1a67487b4f3c748cd95c8c9c2fa
Varduhi <varduhigalstyann71@gmail.com>
2025-02-10 15:11:15 +0400

    vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvArrays

--- index.php
+++ index.php
@@ -1,34 +1,46 @@
 <?php 
-    $radius = 25;
-    $pi = 3.14;
+//indexed arrays
+    $peopleOne = ['shaun', 'crystal', 'ryu'];
+    // echo $peopleOne[1];
 
-    //basic operators - *, /, +, -, ^
-    // echo $pi * $radius^2;
+    $peopleTwo = array('ken', 'chun-li');
+    // echo $peopleTwo[1];
 
-    //order of operation ( B I D M A S )
-    // echo 2*(4+9)/3;
+    $ages = [20, 30, 40, 50];
+    // echo $ages[2];
+    // print_r($ages); // բոլոր  տարրերը ըստ համապատասխան դիրքի տպել
+
+    $ages[1] = 25; 
+    // print_r($ages);
+
+    $ages[] =60; //վերջից ավելացրա 60֊ը
+    // print_r($ages);
+
+    array_push($ages, 70, 90); //$ages զանգվածին  ավելացրա այս արժեքը 70֊ը և 90֊ը
+    // print_r($ages);
+
+    // echo count($ages); //զանգվածի երկարություն
+
+    $peopleThree = array_merge($peopleOne, $peopleTwo); //զանգվածները միավորում ենք մի զանգվածի մեջ
+    // print_r($peopleThree);
 
 
-    // echo $radius++; //$radius-ի արժեքը տպի ու արժեքը +1 արա ու էտպես հիշի
-    // echo $radius--; //$radius-ի արժեքը տպի ու արժեքը -1 արա ու էտպես հիշի
+// associative arrays (key(բանալի) & value(արժեք) pairs)
+    $ninjasOne = ['shaun'=> 'black', 'mario' => 'orange', 'luigi' => 'brown'];
+    // echo $ninjasOne['mario'];
+    // print_r ($ninjasOne);
 
-    // echo $radius; //$radius-ի արժեքը  արդեն ++ կամ -- արածն է
+    $ninjasTwo = array('bowser'=>'green', 'peach'=> 'yellow');
+    // print_r($ninjasTwo);
+
+    $ninjasTwo['toad'] = 'pink'; //$ninjasTwo զանգվածում ավելացվում է
+    $ninjasTwo['peach'] = 'red'; //$ninjasTwo զանգվածի peach տարիի yellowի փոխարեն վերագրում է red
+    // print_r($ninjasTwo);
     
-
-    $age = 20;
-    // $age +=10;
-    // $age -=4;
-    // $age *=2;
-    // echo $age;
-
-
-    // number 
-
-    // echo floor($pi); //$pi-ի թիվը գրի դեպի ներքև կլորացրած  ամբողջ թվի տեսքով
-    // echo ceil($pi); //դեպի վերև կլորացրած
-
-    echo pi();// վեերադարձնում է pi֊ի արժեքը, ոռը կապ չունի $pi-ի
-    
+    // echo count($ninjasOne); //զանգվածի երկարություն
+     
+    $ninjasThree = array_merge($ninjasOne, $ninjasTwo);
+    print_r($ninjasThree);
 ?>
 
 <!DOCTYPE html>
