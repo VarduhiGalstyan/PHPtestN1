4efddb6ac43bf2b953901d8a1d9da716bf79e868
Varduhi <varduhigalstyann71@gmail.com>
2025-02-10 12:57:59 +0400

    echo -ի մանրամասն ուսումնասիրում

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php 
-    define('NAME', 'Eva');
-    $name = "Yoshi";
-    $age = 30;
-    echo $name;
-
+    $stringOne = 'my email is ';
+    $stringTwo = 'mario123@thenetnja.co.uk';
     $name = 'Mario';
+
+    // echo $stringOne . $stringTwo ;
+
+    // echo 'Hey, my name is' . $name ;
+    // echo "Hey my name is $name. ";
+
+    // echo 'Hey my name is $name';
+
+    // echo "The ninja screamed \"whaaaa\". ";
+    // echo 'The ninja screamed "whaaaa". ';
+
+    // echo $name[1];
+    // echo $name[0];
+    // echo $name[8];
+
+    // echo strlen($name); //$name-ի տարերի քանակն է վերադարձնում
+    
+    // echo strtoupper($name); //$name-ի բոլոր տառերը գրի մեծատառ
+    // echo strtolower($name); //$name-ի բոլոր տառերը գրի փոքրատառ
+
+    echo str_replace('M', 'W', $name); // M տառը փոխարինի W-ով $name-ում
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +31,6 @@
         <title>PHP Tutorials</title>
     </head>
     <body>
-        <h1>User Profile Page</h1>
-        <div><?php echo NAME; ?></div>
-        <div><?php echo $name; ?></div>
-        <div><?php echo $age;?></div>
+        
     </body>
 </html>
