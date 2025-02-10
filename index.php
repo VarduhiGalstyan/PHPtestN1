b690e0b188ee23c2ef19e4e84bb0d9182495066c
Varduhi <varduhigalstyann71@gmail.com>
2025-02-10 11:43:40 +0400

    echo

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    echo 'hello, ninjas';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>my first PHP file: PHPtest</title>
+    </head>
+    <body>
+        <h1>User Profile Page</h1>
+        <h1><?php echo 'hello, ninjeas'; ?></h1>
+    </body>
+</html>
