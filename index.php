cfca6523340438ebe4d10c98c73856587393d8a6
Varduhi <varduhigalstyann71@gmail.com>
2025-02-10 15:45:57 +0400

    Multidimensional Arrays

--- index.php
+++ index.php
@@ -1,46 +1,28 @@
 <?php 
-//indexed arrays
-    $peopleOne = ['shaun', 'crystal', 'ryu'];
-    // echo $peopleOne[1];
+//multi-dimensional arrays
+    // $blogs = [
+    //     ['mario party', 'mario', 'lorem', 30],
+    //     ['mario kart cheats', 'toad', 'lorem', 25],
+    //     ['zelda hidden chests', 'link', 'lorem', 50],
+    // ];
+    // print_r($blogs);
+    // print_r($blogs[1]);
+    // print_r($blogs[1][1]);
 
-    $peopleTwo = array('ken', 'chun-li');
-    // echo $peopleTwo[1];
+    $blogs = [
+        ['title' => 'mario party','author' => 'mario','content' => 'lorem', 'likes' => 30],
+        ['title' => 'mario kart cheats','author' =>  'toad','content' => 'lorem', 'likes' => 25],
+        ['title' => 'zelda hidden chests','author' =>  'link','content' => 'lorem','likes' => 50],
+    ];
+    // print_r($blo/gs[2]['author']);
+    // echo $blogs[2]['author'];
+    // echo count($blogs);
 
-    $ages = [20, 30, 40, 50];
-    // echo $ages[2];
-    // print_r($ages); // բոլոր  տարրերը ըստ համապատասխան դիրքի տպել
+    $blogs[] = ['title' => 'castle party', 'author' => 'peach', 'content' => 'lorem', 'likes' => 100];
+    // print_r($blogs);
 
-    $ages[1] = 25; 
-    // print_r($ages);
-
-    $ages[] =60; //վերջից ավելացրա 60֊ը
-    // print_r($ages);
-
-    array_push($ages, 70, 90); //$ages զանգվածին  ավելացրա այս արժեքը 70֊ը և 90֊ը
-    // print_r($ages);
-
-    // echo count($ages); //զանգվածի երկարություն
-
-    $peopleThree = array_merge($peopleOne, $peopleTwo); //զանգվածները միավորում ենք մի զանգվածի մեջ
-    // print_r($peopleThree);
-
-
-// associative arrays (key(բանալի) & value(արժեք) pairs)
-    $ninjasOne = ['shaun'=> 'black', 'mario' => 'orange', 'luigi' => 'brown'];
-    // echo $ninjasOne['mario'];
-    // print_r ($ninjasOne);
-
-    $ninjasTwo = array('bowser'=>'green', 'peach'=> 'yellow');
-    // print_r($ninjasTwo);
-
-    $ninjasTwo['toad'] = 'pink'; //$ninjasTwo զանգվածում ավելացվում է
-    $ninjasTwo['peach'] = 'red'; //$ninjasTwo զանգվածի peach տարիի yellowի փոխարեն վերագրում է red
-    // print_r($ninjasTwo);
-    
-    // echo count($ninjasOne); //զանգվածի երկարություն
-     
-    $ninjasThree = array_merge($ninjasOne, $ninjasTwo);
-    print_r($ninjasThree);
+    $popped = array_pop($blogs);
+    print_r($popped);
 ?>
 
 <!DOCTYPE html>
