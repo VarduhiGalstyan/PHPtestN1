f5838fe191a847915f9cd5dc327f259bebfaaeda
Varduhi <varduhigalstyann71@gmail.com>
2025-02-11 17:09:51 +0400

    #20 Filters & More Validation

--- add.php
+++ add.php
@@ -1,34 +1,37 @@
 <?php  
-    // if(isset($_GET['submit'])){
-    //     echo $_GET['email'];
-    //     echo $_GET['title'];
-    //     echo $_GET['ingredients'];
-    // }
-
-    if(isset($_POST['submit'])){
-        // echo htmlspecialchars ($_POST['email']);
-        // echo htmlspecialchars ($_POST['title']);
-        // echo htmlspecialchars ($_POST['ingredients']);
+    if(isset($_POST['submit'])){ //եթե սեխմված է
 
      //check email
-        if(empty($_POST['email'])){
+        if(empty($_POST['email'])){ //եթե դատարկ է
             echo 'An email is required <br/>';
         } else {
-            echo htmlspecialchars($_POST['email']);
+            // echo htmlspecialchars($_POST['email']);
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo 'email must be a valid email address';
+            }
         }
 
      //check title
         if(empty($_POST['title'])){
             echo 'A title is required <br/>';
         } else {
-            echo htmlspecialchars($_POST['title']);
+            // echo htmlspecialchars($_POST['title']);
+            $title = $_POST['title'];
+            if(preg_match('/^[a-zA-Z\s]+$/', $title)){
+                echo 'TITLE MUST BE LETTERS AND SPACES ONLY.';
+            }
         }
 
      //check ingredients
         if(empty($_POST['ingredients'])){
             echo 'At least one ingredient is required <br/>';
         } else {
-            echo htmlspecialchars($_POST['ingredients']);
+            // echo htmlspecialchars($_POST['ingredients']);
+            $ingredients= $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                echo 'Ingredients must be a comma separated list';
+            }
         }
 
     } // end of POST check
