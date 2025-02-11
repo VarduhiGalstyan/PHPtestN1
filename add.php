6294c9bad75847e0c004660cfc4c6dc62a16f887
Varduhi <varduhigalstyann71@gmail.com>
2025-02-11 15:17:38 +0400

    GET-POST Forms in PHP

---
+++ add.php
@@ -0,0 +1,41 @@
+<?php  
+    // if(isset($_GET['submit'])){
+    //     echo $_GET['email'];
+    //     echo $_GET['title'];
+    //     echo $_GET['ingredients'];
+    // }
+
+    if(isset($_POST['submit'])){
+        echo $_POST['email'];
+        echo $_POST['title'];
+        echo $_POST['ingredients'];
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html>
+    
+    <?php include ('templates/header.php'); ?>
+
+    <section class="container grey-text">
+        <h4 class="center">Add a Pizza</h4>
+        <!-- <form action="add.php" class="white" method="GET"> -->
+        <form action="add.php" class="white" method="POST">
+            <label >Your Email:</label>
+            <input type="text" name ="email">
+            <label >Your Title:</label>
+            <input type="text" name ="title">
+            <label >Ingredients (comma separted):</label>
+            <input type="text" name ="ingerdients">
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+
+    <?php include ('templates/footer.php'); ?>
+    <!-- <body> -->
+        
+    <!-- </body> -->
+</html>
--- templates/header.php
+++ templates/header.php
@@ -9,6 +9,11 @@
         .brand-text{
             color: #cbb09c !important;
         }
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
     </style>
 </header>
 <body class = "grey lighten-4">
