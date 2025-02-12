46fbb255d3026bbe822ceca2c844ae23d87604b6
Varduhi <varduhigalstyann71@gmail.com>
2025-02-12 10:23:59 +0400

    #25 - Connecting to a Database

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php  
+    //  connect to database
+    $conn = mysqli_connect('localhost', 'root', 'admin', 'ninja_pizza');
     
+    //  check connection
+    if(!$conn){
+        echo 'Connection error: ' .mysqli_connect_error();
+    }
 ?>
 
 <!DOCTYPE html>
