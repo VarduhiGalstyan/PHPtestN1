5ad8fbe37d6eb8d8eb295357420ca64546b26c1a
Varduhi <varduhigalstyann71@gmail.com>
2025-02-12 22:58:13 +0400

    #36 - Sessions

--- sandbox.php
+++ sandbox.php
@@ -1,15 +1,14 @@
 <?php
 
-    // superglobals
+    // sessions
+    if(isset($_POST['submit'])){
+        session_start();
+        $_SESSION['name'] = $_POST['name'];
 
-    // $_GET['name'], $POST['name']
+        // echo $_SESSION['name'];
+        header('Location: index.php');
+    }
 
-    echo $_SERVER['SERVER_NAME'] . '<br />';
-    echo $_SERVER['REQUEST_METHOD'] . '<br />';
-    echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
-    echo $_SERVER['PHP_SELF'] . '<br />';
-
-    // $_SERVER, $_COOKIE
 
 ?>
 
@@ -20,10 +19,11 @@
 </head>
 <body>
 
-    <p><?php echo  $score > 40 ? 'high score' : 'low score :('; ?> </p>
-
-    
-
+    <!-- <form  action="sandbox.php"> -->
+    <form action="<?php echo $_SERVER['php_self']?>" method="POST">
+        <input type="text" name="name">
+        <input type="submit" name= "submit" value="submit">
+    </form>
 
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,16 @@
-<header>
+<?php 
+    session_start();
+
+    // $_SESSION['name'] = 'yoshi';
+
+    if($_SERVER['QUERY_STRING'] == 'noname'){
+        // unset($_SESSION['name']);
+        session_unset();
+    }
+
+    $name = $_SESSION['name'];
+?>
+<head>
     <title>Ninja Pizza</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
@@ -23,12 +35,13 @@
             top: -30px;
         }
     </style>
-</header>
+</head>
 <body class = "grey lighten-4">
     <nav class="white z-depth-0">
         <div class="container">
             <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
                 <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
             </ul>
         </div>
